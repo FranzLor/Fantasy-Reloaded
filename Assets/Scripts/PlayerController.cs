83dd13387a1a7d4b39e106fec5488ef5d34edd47
Franz Lor <FranzLor64@yahoo.com>
2024-12-18 23:07:40 -0700

    Player Controller
    player controls and movement foundation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,5 +7,42 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float moveSpeed = 1.0f;
 
+    private PlayerControls playerControls;
+    private Vector2 movement;
+    private Rigidbody2D rigidBody;
 
+
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void Update()
+    {
+        PlayerInput();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void PlayerInput()
+    {
+        movement = playerControls.Movement.Move.ReadValue<Vector2>();
+
+    }
+
+    private void Move()
+    {
+        rigidBody.MovePosition(rigidBody.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    }
 }
