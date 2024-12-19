352062705bb34c605806a4e647454ec80c711309
Franz Lor <FranzLor64@yahoo.com>
2024-12-18 22:42:07 -0700

    Player Controller & Input
    added player controller and input system update

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 1.0f;
+
+
+}
