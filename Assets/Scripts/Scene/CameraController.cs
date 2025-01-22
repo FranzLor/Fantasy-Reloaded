c786fbe928106fceada919e3f18042025d054f09
FranzLor <FranzLor64@yahoo.com>
2025-01-21 19:50:09 -0700

    Scene Camera
    fixes camera not following player on scene transition

---
+++ Assets/Scripts/Scene/CameraController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class CameraController : Singleton<CameraController>
+{
+    private CinemachineCamera cinemachineCamera;
+
+    public void SetPlayerCameraFollow()
+    {
+        cinemachineCamera = FindAnyObjectByType<CinemachineCamera>();
+        cinemachineCamera.Follow = PlayerController.Instance.transform;
+    }
+}
